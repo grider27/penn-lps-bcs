8563780a8ca5b9ad4d04ed79b1b838fd7dd73425
Gary B <grider27@gmail.com>
2021-01-13 15:49:11 -0500

    html dev

--- homework/homework4-CodeQuiz/assets/js/script.js
+++ homework/homework4-CodeQuiz/assets/js/script.js
@@ -3,19 +3,40 @@ var welcomeEl = document.getElementById("welcome");
 var startQuiz = document.getElementById("start-quiz");
 
 var questionEl = document.getElementById("questions")
-var questionTitle = document.getElementById("questionTitle");
+var questionTitle = document.getElementById("question-title");
 var choice1 = document.getElementById("choice1");
 var choice2 = document.getElementById("choice2");
 var choice3 = document.getElementById("choice3");
 var choice4 = document.getElementById("choice4");
+var lineBreak = document.getElementById("lineBreak");
+var resultCheck = document.getElementById("resultCheck");
+
+var scoreEl = document.getElementById("scores");
+var finalScore = document.getElementById("final-score");
+var saveScoreBtn = document.getElementById("submit-score");
+
+var leaderBoardEl = document.getElementById("leader-board");
 
 
 // script variables
 
 
 
+//quiz question
+/*
+cont questions = [
+    {
+        q: ""
+
+    }
+]
+*/
+
+
 // event listeners
 startQuiz.addEventListener("click",initiateQuiz);
+choice1.addEventListener("click",displayFinalScore); //needs to be pointed to right function
+saveScoreBtn.addEventListener("click",leaderBoard());
 
 
 
@@ -28,7 +49,10 @@ function init(){
 }
 
 function welcome(){
+    welcomeEl.style.display = "block";
     questionEl.style.display = "none";
+    scoreEl.style.display = "none";
+    leaderBoardEl.style.display = "none";
     var t = document.getElementById("welcome-title");
     var tx = document.getElementById("welcome-text");
     t.textContent = "Javascript Coding Challenge 2";
@@ -39,9 +63,30 @@ function welcome(){
 function initiateQuiz(){
     welcomeEl.style.display = "none";
     questionEl.style.display = "block";
+    lineBreak.style.display = "none";
+    resultCheck.style.display = "none";
+
     questionTitle.textContent = "question 1";
     choice1.textContent = "option1";
     choice2.textContent = "option2";
     choice3.textContent = "option3";
     choice4.textContent = "option4";
+}
+
+function displayFinalScore(){
+    welcomeEl.style.display = "none";
+    questionEl.style.display = "none";
+    lineBreak.style.display = "none";
+    resultCheck.style.display = "none";
+    scoreEl.style.display = "block";
+}
+
+function leaderBoard(){
+    leaderBoardEl.style.display = "block";
+    welcomeEl.style.display = "none";
+    questionEl.style.display = "none";
+    lineBreak.style.display = "none";
+    resultCheck.style.display = "none";
+    scoreEl.style.display = "none";
+    
 }
