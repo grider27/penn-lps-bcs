feb36769fe3b7ebf6ac274dfa2be01dacd3670d7
Gary B <grider27@gmail.com>
2021-01-22 14:18:44 -0500

    finished the time slots formatting content and values

--- homework/homework5-WorkDayPlanner/script.js
+++ homework/homework5-WorkDayPlanner/script.js
@@ -5,28 +5,30 @@ $(document).ready(function () {
 var today = moment().format('dddd, MMMM Do');
 $('#currentDay').text(today);
 var hrToDisplay = ['9','10','11','12','13','14','15','16','17']
-currentHour = moment().format('H');
-
-//var textBlock = $('<div>');
-//textBlock.addClass("row time-block");
-//textBlock.text("text");
-//textBlock.appendTo('.container');
+currentHour = parseInt(moment().format('H'));
 
 $.each(hrToDisplay,function(i, val){
     var hourRow = $('<div>');
     hourRow.addClass('row time-block');
     hourRow.attr('hour-slot',val);
 
+    // hour column
     var hourCol = $('<div>');
     hourCol.addClass('col-1 hour pt-3');
     if(val < 12){
     hourVal = val+' AM';
-    } else {
+    } 
+    else if(val == 12){
         hourVal = val+' PM';
+        } 
+    else {
+        valNonMil = val-12;
+        hourVal = valNonMil+' PM';
     }
     hourCol.text(hourVal);
     hourRow.append(hourCol);
 
+    //input column
     var inputCol = $('<textarea>');
     inputCol.addClass('col-10 textarea description pt-3');
     if(val < currentHour){
@@ -38,19 +40,18 @@ $.each(hrToDisplay,function(i, val){
         else {
             inputCol.addClass('present');
         }
+
     inputCol.attr('placeholder','Click here to enter')
     hourRow.append(inputCol);
 
+    //save button column
     var saveBtn = $('<button>');
-    saveBtn.addClass('col-1 saveBtn');
-    saveBtn.text(val);
+    saveBtn.addClass('col-1 saveBtn fa fa-save');
     hourRow.append(saveBtn);
 
+    // append completed rows to the container
     hourRow.appendTo('.container');
 });
-
-currentHour = moment().format('H');
-console.log(currentHour);
 
 
 });
