f1832466b1b23c5233b520b7d81ca4ae21398395
Gary B <grider27@gmail.com>
2021-01-25 14:55:31 -0500

    weather api work and testing to console results

--- homework/homework6-WeatherDashboard/assets/script.js
+++ homework/homework6-WeatherDashboard/assets/script.js
@@ -1,6 +1,6 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     // hide the results section on load
-    $('#results').toggleClass('d-none'); 
+    $('#results').toggleClass('d-none');
 
     var appID = "d0b9142c750e110a89c37e7297e98e02";
     var today = moment().format('L');
@@ -9,10 +9,10 @@ $( document ).ready(function() {
     var prevSearches = localStorage.getItem('storedCities');
     if (prevSearches != null) {
         prevSearchesResults = JSON.parse(prevSearches);
-        console.log(prevSearchesResults);
+        //console.log(prevSearchesResults);
         $.each(prevSearchesResults, function (i, val) {
             var liItem = $('<li>');
-            liItem.attr('id','historicalSearches');
+            liItem.attr('id', 'historicalSearches');
             liItem.addClass('list-group-item');
             liItem.text(val.cityOf);
             $('#prev-searches').append(liItem);
@@ -23,10 +23,10 @@ $( document ).ready(function() {
     $('.list-group-item').on('click', function () {
         var cityOf = $(this).text();
         console.log(cityOf);
-        if (cityOf != ""){
-        getWeather(cityOf);
+        if (cityOf != "") {
+            getWeather(cityOf);
         }
-        else {
+        else { // validation checks and prompts
             alert("Please enter a city name first for the search");
         }
     });
@@ -34,27 +34,40 @@ $( document ).ready(function() {
     // initiate search with a new city name
     $('#search-city').on('click', function () {
         var cityOf = $('#enter-city').val();
-        if (cityOf != ""){
-        $('#enter-city').val("");
-        var liItem = $('<li>');
-        liItem.attr('id','historicalSearches');
-        liItem.addClass('list-group-item');
-        liItem.text(cityOf);
-        $('#prev-searches').append(liItem);
-        var savedSearhes = JSON.parse(localStorage.getItem('storedCities')) || [];
-        savedSearhes.push({ cityOf: cityOf });
-        localStorage.setItem('storedCities', JSON.stringify(savedSearhes));
-        getWeather(cityOf);
+        if (cityOf != "") {
+            $('#enter-city').val("");
+            var liItem = $('<li>');
+            liItem.attr('id', 'historicalSearches');
+            liItem.addClass('list-group-item');
+            liItem.text(cityOf);
+            $('#prev-searches').append(liItem);
+            var savedSearhes = JSON.parse(localStorage.getItem('storedCities')) || [];
+            savedSearhes.push({ cityOf: cityOf });
+            localStorage.setItem('storedCities', JSON.stringify(savedSearhes));
+            getWeather(cityOf);
         }
         else {
             alert("Please enter a city name first for the search");
         }
     });
 
-function getWeather(city){
-    var requestURl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + appID;
-    //console.log(requestURl);
-}
+    function getWeather(city) {
+        var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=imperial"+"&APPID=" + appID;
+        //console.log(requestURl);
+        fetch(requestUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data); //.wind.speed);
+                var cityName = data.name;
+                var cityTemp = data.main.temp;
+                var cityHumidity = data.main.humidity;
+                var cityWind = data.wind.speed;
+                var cityIcon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+                //console.log(cityIcon);
+            });
+    }
 
 
 });
