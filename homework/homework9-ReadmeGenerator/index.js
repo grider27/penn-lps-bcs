6d412c5ac2881b5d69485201ca12e08d12fb029a
Gary B <grider27@gmail.com>
2021-02-18 17:32:44 -0500

    added questions and initial attempt at JSON stringify a file

--- homework/homework9-ReadmeGenerator/index.js
+++ homework/homework9-ReadmeGenerator/index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inq = require('inquirer');
+const fs = require('fs');
+const genMD = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of your project:',
+        validate: (value) => value ? true : "Please enter a proper title for the project"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter the description of your project:',
+        validate: (value) => value ? true : "Please enter a proper description for the project"
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter the installation instructions of your project:',
+        validate: (value) => value ? true : "Please enter a proper installation instructions for the project"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter the examples for use of your project:',
+        validate: (value) => value ? true : "Please enter a proper examples for the project"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project from the following:',
+        choices: ["MIT", "GNU GPLv3"],
+        validate: (value) => value ? true : "Please choose a license for your project"
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please enter information of any contributors to your project:',
+        validate: (value) => value ? true : "Please enter a proper contributors or enter NA "
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter information on how to test your project:',
+        validate: (value) => value ? true : "Please enter a proper information on testing or enter NA"
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your github username:',
+        validate: (value) => value ? true : "Please enter a proper information on testing or enter NA"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address:',
+        validate: (value) => value ? true : "Please enter a proper email address"
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { 
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Successful creation of a README file! Please check the directory")
+    );
+};
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inq.prompt(questions).then((data) => {
+        //console.log(data)
+        writeToFile("README.md", JSON.stringify(data, null, '\t'));
+    });
+};
 // Function call to initialize app
 init();
