28eadc2c8d01f320c50d883ba166d11469b56cce
Gary B <grider27@gmail.com>
2021-02-28 10:19:38 -0500

    successful questions walk through for all employees and temp write out to local file

--- homework/homework10-TeamProfileGenerator/index.js
+++ homework/homework10-TeamProfileGenerator/index.js
@@ -4,22 +4,82 @@ const fs = require('fs');
 
 // local dependencies
 const questions = require('./src/questions');
-//const Manager = require('./lib/Manager');
-//const Engineer = require('./lib/Engineer');
-//const Intern = require('./lib/Intern');
-//const genHTML = require('./src/generateHTML.js');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const genHTML = require('./src/generateHTML.js');
+const staff = [];
+
+// employee type question
+function getEmployeeType() {
+    inquirer.prompt(questions.employeeType)
+        .then((selection) => {
+            switch (selection.employee) {
+                case "Manager":
+                    console.log("test worked");
+                    getManager();
+                    break;
+                case "Engineer":
+                    getEngineer();
+                    break;
+                case "Intern":
+                    getIntern();
+                    break;
+                case "Done":
+                    generateHTML();
+                    break;
+            }
+        });
+
+}
+
+// capture manager details
+function getManager() {
+    inquirer.prompt(questions.managerDetails)
+        .then((details) => {
+            let manager = new Manager(details.name, details.id, details.email, details.officeNumber);
+            staff.push(manager)
+        })
+        .then(() => {
+            getEmployeeType();
+        })
+}
+
+// capture manager details
+function getIntern() {
+    inquirer.prompt(questions.internDetails)
+        .then((details) => {
+            let intern = new Intern(details.name, details.id, details.email, details.school);
+            staff.push(intern)
+        })
+        .then(() => {
+            getEmployeeType();
+        })
+}
+
+// capture engineer details
+function getEngineer() {
+    inquirer.prompt(questions.engineerDetails)
+        .then((details) => {
+            let engineer = new Engineer(details.name, details.id, details.email, details.github);
+            staff.push(engineer)
+        })
+        .then(() => {
+            getEmployeeType();
+        })
+}
+
+function generateHTML() {
+    fs.writeFile("test.txt", JSON.stringify(staff, null, '\t'), (err) =>
+        err ? console.log(err) : console.log("Successful creation of a HTML file! Please check the directory under dist folder")
+    );
+}
 
 // function to initialize app
 function init() {
     console.log(`Welcome to Team Profile Generator!`);
     console.log(`Let me ask you a few questions to get your started:`);
-    inquirer.prompt(questions.employeeType).then((selection) => {
-        console.log("questions fired");
-        fs.writeFile("test.txt", JSON.stringify(selection, null, '\t'), (err) =>
-            err ? console.log(err) : console.log("Successful creation of a README file! Please check the directory")
-        );
-    });
-
+    getEmployeeType();
 };
 
 // Function call to initialize app
--- homework/homework10-TeamProfileGenerator/src/questions.js
+++ homework/homework10-TeamProfileGenerator/src/questions.js
@@ -3,9 +3,95 @@ const employeeType = [
         type: 'list',
         name: 'employee',
         message: 'Please choose the type of employee to enter or select Done if you are ready to generate HTML page:',
-        choices: ["Manager", "Engineer", "Intern","Done"],
+        choices: ["Manager", "Engineer", "Intern", "Done"],
         validate: (value) => value ? true : "Please choose a valid option!"
     },
 ]
 
-module.exports = {employeeType};
+const managerDetails = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the Manager's name?",
+        validate: (value) => value ? true : "Please enter a valid name"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the Manager's employee ID?",
+        validate: (value) => value ? true : "Please enter a valid ID"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the Manager's email?",
+        validate: (value) => value ? true : "Please enter a valid email"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the Manager's office phone number?",
+        validate: (value) => value ? true : "Please enter a valid office number"
+    },
+]
+
+const internDetails = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the Intern's name?",
+        validate: (value) => value ? true : "Please enter a valid name"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the Intern's employee ID?",
+        validate: (value) => value ? true : "Please enter a valid ID"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the Intern's email?",
+        validate: (value) => value ? true : "Please enter a valid email"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the Intern's School name?",
+        validate: (value) => value ? true : "Please enter a valid School name"
+    },
+]
+
+const engineerDetails = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the Engineer's name?",
+        validate: (value) => value ? true : "Please enter a valid name"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the Engineers's employee ID?",
+        validate: (value) => value ? true : "Please enter a valid ID"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the Engineer's email?",
+        validate: (value) => value ? true : "Please enter a valid email"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the Engineers's github username?",
+        validate: (value) => value ? true : "Please enter a valid username"
+    },
+]
+
+module.exports = {
+    employeeType,
+    managerDetails,
+    engineerDetails,
+    internDetails
+};
